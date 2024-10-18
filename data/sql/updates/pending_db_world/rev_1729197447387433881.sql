7180cc8a562102ecb22396e6a4527a21acdc3e19
Jelle Meeus <sogladev@gmail.com>
2024-10-18 16:49:46 +0200

    fixup! add sql

--- data/sql/updates/pending_db_world/rev_1729197447387433881.sql
+++ data/sql/updates/pending_db_world/rev_1729197447387433881.sql
@@ -14,7 +14,7 @@ UPDATE `spell_linked_spell` SET `spell_effect` =38126 WHERE `spell_trigger`=3812
 UPDATE `spell_linked_spell` SET `spell_effect` =38128 WHERE `spell_trigger`=38127;
 UPDATE `spell_linked_spell` SET `spell_effect` =38130 WHERE `spell_trigger`=38129;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 38020;
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES(38020, 38022, 0, 'FRS (Fel Reaver Sentinel) Quest Credit');
-
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(38020, 38022, 0, 'FRS (Fel Reaver Sentinel) Quest Credit');
 DELETE FROM `spell_script_names` WHERE `spell_id` = 38054 AND `ScriptName`='spell_random_rocket_missile';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES(38054, 'spell_random_rocket_missile');
